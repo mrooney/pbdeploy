0f84b53ee27f81d66f63c38bdf961533361daf77
Michael Rooney <mrooney.github@rowk.com>
2013-10-18 22:39:30 -0700

    bump version so we can upload new description to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pbdeploy',
     description='pbdeploy (port-based deploy) is a script that handles starting, reloading and stopping your server processes, without requiring a daemon or pidfiles. It also supports running scripts before and after certain services are loaded, such as installing requirements, performing database migrations, and running tests, to make continuous deployment a breeze.',
-    version='1.4',
+    version='1.5',
     packages=['pbdeploy'],
     scripts=['bin/pbdeploy'],
     license='The MIT License',
